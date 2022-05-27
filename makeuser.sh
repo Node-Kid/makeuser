885f614d6824596615ac6c4e43b3a8e0a854001a
will weiszhaar <weiszhaarwill@gmail.com>
2022-05-27 14:30:03 -0400

    first commit

---
+++ makeuser.sh
@@ -0,0 +1,8 @@
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser UserShell /bin/bash
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser RealName "Auser"
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser NFSHomeDirectory /Local/Target/Local/Users/auser
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser UniqueID 599
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser PrimaryGroupID 80
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -password /Local/Target/Users/auser dcs22
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -append /Local/Target/Groups/admin GroupMembership auser
