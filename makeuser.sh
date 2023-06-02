6188292e4a77fc27bb2d472bf91e9b5405c7191c
Node-Kid <72894582+Node-Kid@users.noreply.github.com>
2023-06-02 13:32:53 -0400

    Change password to passwd

--- makeuser.sh
+++ makeuser.sh
@@ -4,5 +4,5 @@ dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly 
 dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser NFSHomeDirectory /Local/Target/Local/Users/auser
 dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser UniqueID 599
 dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -create /Local/Target/Users/auser PrimaryGroupID 80
-dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -password /Local/Target/Users/auser dcs22
+dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -passwd /Local/Target/Users/auser dcs22
 dscl -f "/Volumes/Macintosh HD/private/var/db/dslocal/nodes/Default/" localonly -append /Local/Target/Groups/admin GroupMembership auser
